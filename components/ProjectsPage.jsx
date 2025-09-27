2f9f14b20ed8bdc3af0af9186314d1d47e8b7ac9
ninadurand <mendesdurandmarina@gmail.com>
2025-09-26 21:32:13 -0300

    Adicionando Ilustrações Digitais

--- components/ProjectsPage.jsx
+++ components/ProjectsPage.jsx
@@ -6,28 +6,60 @@ export default function ProjectsPage() {
     { title: 'Rascunho', image: 'Desenhos/Rascunho.jpeg' },
   ]
 
+     const digitals = [
+    { title: 'Cardume Digital', image:'Desenhos/cardume_digital.jpg' },
+    { title: 'Vrum Vrum', image: 'Desenhos/Vrum_Vrum.jpg' },
+    { title: 'Nosferatu', image: 'Desenhos/Nosferatu.jpg' },
+    { title: 'O -', image: 'Desenhos/O -.jpg' },
+  ]
+
   return (
-    <div className="page-container min-vh-100 bg-purple-dark text-white d-flex align-items-center justify-content-center px-5 pb-5">
-      <div className="container text-center">
-        <h2 className="display-4 fw-bold mb-5 text-pink">Meus Desenhos</h2>
-        <div className="row g-4 justify-content-center">
-          {drawings.map((drawing, index) => (
-            <div key={index} className="col-lg-3 col-md-6">
-              <div className="card h-100 shadow-lg border-0 project-card bg-purple-medium text-white">
-                <img
+  <div className="page-container min-vh-100 bg-purple-dark text-white d-flex flex-column align-items-center justify-content-center px-5 pb-5">
+    
+    {/* GALERIA DOS DESENHOS */}
+    <div className="container text-center mb-5">
+      <h2 className="display-4 fw-bold mb-5 text-pink">Meus Desenhos</h2>
+      <div className="row g-4 justify-content-center">
+        {drawings.map((drawing, index) => (
+          <div key={index} className="col-lg-3 col-md-6">
+            <div className="card h-100 shadow-lg border-0 project-card bg-purple-medium text-white">
+              <img
                 src={drawing.image}
                 alt={drawing.title}
                 className="card-img-top bg-dark"
                 style={{ height: '250px', objectFit: 'contain' }}
-                />
-                <div className="card-body d-flex flex-column">
-                  <h5 className="card-title fw-bold text-pink">{drawing.title}</h5>
-                </div>
+              />
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title fw-bold text-pink">{drawing.title}</h5>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
-  )
+
+    {/* GALERIA DOS DIGITAIS */}
+    <div className="container text-center">
+      <h2 className="display-4 fw-bold mb-5 text-pink">Minhas Ilustrações Digitais</h2>
+      <div className="row g-4 justify-content-center">
+        {digitals.map((drawing, index) => (
+          <div key={index} className="col-lg-3 col-md-6">
+            <div className="card h-100 shadow-lg border-0 project-card bg-purple-medium text-white">
+              <img
+                src={drawing.image}
+                alt={drawing.title}
+                className="card-img-top bg-dark"
+                style={{ height: '250px', objectFit: 'contain' }}
+              />
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title fw-bold text-pink">{drawing.title}</h5>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+
+  </div>
+)
 }
